a4ea9122ff775e14b6b69e7952830ebe6f493bef
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-03-19 02:52:46 +0530

    test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <Button>Get started</Button>
+      <Button>Get started"</Button>
     </div>
   )
 }
